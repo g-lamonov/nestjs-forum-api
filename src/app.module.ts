3673de2351b5b97ac8c4896feb751e93b4d4c3a1
g-lamonov <owsrey@gmail.com>
2021-12-24 21:51:45 +0300

    feat: use 'nest g resource' command to generate rest for the like

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
+import { LikeModule } from './like/like.module';
 
 @Module({
-  imports: [ArticleModule, AuthModule, CategoryModule],
+  imports: [ArticleModule, AuthModule, CategoryModule, LikeModule],
   controllers: [AppController],
   providers: [AppService],
 })
