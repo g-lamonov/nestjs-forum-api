ec902c3ab72f9d03df3b7b5deeb869cba376253e
g-lamonov <owsrey@gmail.com>
2021-12-24 21:50:13 +0300

    feat: use 'nest g resource' command to generate rest for the auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ArticleModule],
+  imports: [ArticleModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
