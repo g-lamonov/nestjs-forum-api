4d25224b4c33b96f901c115b89c15cfe7615d791
g-lamonov <owsrey@gmail.com>
2021-12-24 21:53:31 +0300

    feat: use 'nest g resource' command to generate rest for the comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { LikeModule } from './like/like.module';
 import { TagModule } from './tag/tag.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [ArticleModule, AuthModule, CategoryModule, LikeModule, TagModule],
+  imports: [ArticleModule, AuthModule, CategoryModule, LikeModule, TagModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
