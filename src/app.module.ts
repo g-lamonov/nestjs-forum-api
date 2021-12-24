c96f83d4353ccac9bdf7e1d7db2f965d2d220729
g-lamonov <owsrey@gmail.com>
2021-12-24 22:39:10 +0300

    feat: connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,20 @@ import { CategoryModule } from './category/category.module';
 import { LikeModule } from './like/like.module';
 import { TagModule } from './tag/tag.module';
 import { CommentModule } from './comment/comment.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ArticleModule, AuthModule, CategoryModule, LikeModule, TagModule, CommentModule],
+  imports: [
+    ArticleModule,
+    AuthModule,
+    CategoryModule,
+    LikeModule,
+    TagModule,
+    CommentModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
