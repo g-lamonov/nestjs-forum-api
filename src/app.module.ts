16eb25220c90862f3ca081474f4c881a4df4a888
g-lamonov <owsrey@gmail.com>
2021-12-24 21:50:38 +0300

    feat: use 'nest g resource' command to generate rest for the category

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
 import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [ArticleModule, AuthModule],
+  imports: [ArticleModule, AuthModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
